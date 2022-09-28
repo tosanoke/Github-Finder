49b0dc544bbfdd0ee4b6264ad87fbcd8887e7891
tosanoke <tosanoke@gmail.com>
2022-09-28 18:01:57 +0100

    removed .env from github

--- src/components/users/Users.tsx
+++ src/components/users/Users.tsx
@@ -1,22 +1,26 @@
-import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { axiosPrivate } from "../../network/axios.config";
 import { RequestResponse } from "../../types";
 
 export const Users = () => {
+
+  const [users, setUsers] = useState([])
+  const [isLoading, setIsLoading] = useState(true);
+
   const userApiRequest = async () => {
     const response: RequestResponse = await axiosPrivate.get(
       `${import.meta.env.VITE_API_URL}/users`);
-      return response.data;
+      setUsers(response.data);
+      setIsLoading(false)
   };
-
+ 
   useEffect(() => {
     userApiRequest()
   }, []);
 
   return (
-      <>
+      <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2" >
         Users
-      </>
+      </div>
   );
 };
